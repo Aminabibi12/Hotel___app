c6629cbdd9403b6e8cefab91265cc5d4466b9c39
occ-113 <occ-113@occ-113s-MacBook-Pro.local>
2024-02-24 00:02:59 +0100

    img-update

--- hotel/src/components/About.js
+++ hotel/src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section className="about" id="about" style={{marginTop:"100px"}}>
       <div className="about-img">
-        <img src="http://www.elathemes.com/themes/colina/assets/images/slide-2.jpg" alt="" />
+        <img src="https://img.freepik.com/free-photo/female-receptionist-elegant-suit-during-work-hours_23-2149714393.jpg?w=1480&t=st=1708729334~exp=1708729934~hmac=27e04e7ccf47f6aa3ec2c22f1e0f92c559012597b53f3dd1923a08d78ca31c63" alt="" />
       </div>
       <div className="about-text">
         <span>About Us</span>
