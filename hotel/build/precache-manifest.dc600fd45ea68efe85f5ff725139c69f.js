664c2bdc139b5276dfe8f16f39a451ab4586e8e1
occlaptop L0023 <occlaptopl0023@occlaptops-MacBook-Pro.local>
2024-12-10 16:20:14 +0100

    committed

---
+++ hotel/build/precache-manifest.dc600fd45ea68efe85f5ff725139c69f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d27eb2c48fd6bc89dcd37db5992fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10780a2e8d0a7aed3259",
+    "url": "/static/css/main.c941d211.chunk.css"
+  },
+  {
+    "revision": "18dde6d747bf116f0e7a",
+    "url": "/static/js/2.90aedb87.chunk.js"
+  },
+  {
+    "revision": "ba0bfa8f95d9766194b5",
+    "url": "/static/js/3.7426b29a.chunk.js"
+  },
+  {
+    "revision": "10780a2e8d0a7aed3259",
+    "url": "/static/js/main.428fb59e.chunk.js"
+  },
+  {
+    "revision": "3dfe5f0477b66a36e469",
+    "url": "/static/js/runtime~main.a1b9b0b5.js"
+  }
+]);
