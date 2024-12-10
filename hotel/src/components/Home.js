4ad1a706c27db5a91c66ede1e86249796deaa1ab
isha789 <97890128+isha789@users.noreply.github.com>
2024-12-10 15:16:44 +0100

    Update Home.js

--- hotel/src/components/Home.js
+++ hotel/src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
       <div className="home-text">
         <h1>Treat yourself to everyday exceptional.</h1>
         <h2>Enjoy The Whispering Pines, which is open all day, as well as Bar Brasserie Montbrie, where quality, workmanship, and service are prioritized.</h2>
-        <a href="#" className="btn">Explore More</a>
+        <a href="#" className="btn">Explore More!</a>
       </div>
       <div className="home-img">
         <img src="https://assets.website-files.com/63d0fad7fc0a8ee33b7ee8d0/63d3937a34f30223ed47eb54_Treat-large.png" alt="" />
